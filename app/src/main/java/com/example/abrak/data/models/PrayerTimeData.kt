4f16bb79b8237f2c5895cd1cbe88f3276c8bd660
Mohadese Choupani <mohadeseh.choupani@toucantg.com>
2025-01-29 12:50:00 +0330

    Adds prayer time data class

---
+++ app/src/main/java/com/example/abrak/data/models/PrayerTimeData.kt
@@ -0,0 +1,14 @@
+package com.example.abrak.data.models
+data class PrayerTimeData(
+    var city: String? = null,
+    var azanSobh: String? = null,
+    var toloeAftab: String? = null,
+    var azanZohre: String? = null,
+    var ghorobAftab: String? = null,
+    var azanMaghreb: String? = null,
+    var nimeShabeSharie: String? = null,
+    var month: String? = null,
+    var day: String? = null,
+    var longitude: String? = null,
+    var latitude: String? = null
+)
